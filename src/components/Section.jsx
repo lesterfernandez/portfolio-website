55e0519b068b323c27cf57cc60fc12a5c3b80922
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-11-28 20:53:11 -0500

    removed animation for mobile

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,30 +1,43 @@
 import { Container } from "@chakra-ui/layout";
+import { useMediaQuery } from "@chakra-ui/media-query";
 import { motion } from "framer-motion";
 import { Outlet } from "react-router";
 
 const MotionContainer = motion(Container);
 
 const variants = {
-  hidden: { opacity: 0, x: 100 },
+  hidden: { opacity: 0, x: 75 },
   enter: { opacity: 1, x: 0 },
-  exit: { opacity: 0, x: -100 },
+  exit: { opacity: 0, x: -75 },
 };
 
 const Section = () => {
-  return (
-    <MotionContainer
-      maxW="container.sm"
-      py="1rem"
-      initial="hidden"
-      animate="enter"
-      exit="exit"
-      variants={variants}
-      sx={{ position: "relative", overflow: "hidden" }}
-      transition={{ duration: 0.4, type: "easeInOut" }}
-    >
-      <Outlet />
-    </MotionContainer>
-  );
+  const isLargeScreen = useMediaQuery("(min-width: 800px)")[0];
+  if (isLargeScreen)
+    return (
+      <MotionContainer
+        maxW="container.sm"
+        py="1rem"
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        variants={variants}
+        sx={{ position: "relative", overflow: "hidden" }}
+        transition={{ duration: 0.4, type: "easeInOut" }}
+      >
+        <Outlet />
+      </MotionContainer>
+    );
+  else
+    return (
+      <Container
+        maxW="container.sm"
+        py="1rem"
+        sx={{ position: "relative", overflow: "hidden" }}
+      >
+        <Outlet />
+      </Container>
+    );
 };
 
 export default Section;
