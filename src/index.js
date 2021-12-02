d24bf207e841e87b02713aacc08838910f020e05
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-12-02 10:20:24 -0500

    switched to hash router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ChakraProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
       </ChakraProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
